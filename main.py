ae00fa3134091efe94621551b867515adf121485
binaerverkehr <65082683+binaerverkehr@users.noreply.github.com>
2026-09-01 16:41:58 +0200

    Zeichnen

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ import pygame
 pygame.init()
 
 window = pygame.display.set_mode((800, 600))
+x = 0
+y = 0
 
 running = True
 while running:
-
+    # Ereignisse prüfen
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # Spielzustand berechnen
+    x += 1
+
+    # Render
+    window.fill("white")
+    rect = pygame.Rect((x, y), (100, 100))
+    pygame.draw.rect(window, "#8c2020", rect)
+    pygame.draw.circle(window, "blue", (100, 100), 100)
+
+    pygame.display.flip()
 
 pygame.quit()
