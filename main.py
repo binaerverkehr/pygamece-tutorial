7dc20c4f9cd5cc0a4a8a37e2cdf74f8226b422a5
binaerverkehr <65082683+binaerverkehr@users.noreply.github.com>
2026-09-03 13:53:20 +0200

    1. Einzelbewegung mit KEYDOWN

--- main.py
+++ main.py
@@ -12,10 +12,18 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                y -= 10
+            elif event.key == pygame.K_s:
+                y += 10
+            elif event.key == pygame.K_a:
+                x -= 10
+            elif event.key == pygame.K_d:
+                x += 10
 
     # Spielzustand berechnen
     
-
     # Render
     window.fill("white")
 
