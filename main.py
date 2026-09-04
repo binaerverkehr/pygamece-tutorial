a037b467b8829206fddec1484d29f62deb033d41
binaerverkehr <65082683+binaerverkehr@users.noreply.github.com>
2026-09-04 15:31:54 +0200

    Steuerungen

--- main.py
+++ main.py
@@ -6,24 +6,39 @@ window = pygame.display.set_mode((800, 600))
 x = window.get_width() / 2
 y = window.get_height() / 2
 
+
 running = True
 while running:
-    # Ereignisse prüfen
+    # Events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
+        # Ereignisbasierte Steuerung
+        """
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                y -= 10
-            elif event.key == pygame.K_s:
-                y += 10
-            elif event.key == pygame.K_a:
-                x -= 10
-            elif event.key == pygame.K_d:
-                x += 10
+                y -= 50
+            if event.key == pygame.K_s:
+                y += 50
+            if event.key == pygame.K_a:
+                x -= 50
+            if event.key == pygame.K_d:
+                x += 50
+        """
 
-    # Spielzustand berechnen
-    
+    # Zustandsbasierte Steuerung
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        y -= 1
+    if keys[pygame.K_s]:
+        y += 1
+    if keys[pygame.K_a]:
+        x -= 1
+    if keys[pygame.K_d]:
+        x += 1
+
+    # Update
+
     # Render
     window.fill("white")
 
@@ -32,3 +47,5 @@ while running:
     pygame.display.flip()
 
 pygame.quit()
+
+
